741d2987b0ef9a939748cffa4ebdfbe948a140d8
Lewis Nozaki <lhnozaki@gmail.com>
2019-10-05 11:43:46 -1000

    passed tests

--- SudokuValidator.js
+++ SudokuValidator.js
@@ -10,10 +10,25 @@
 
 exports.SudokuValidator = SudokuValidator;
 
-function SudokuValidator(sudoku){
+function SudokuValidator(sudoku) {
   this.sudoku = sudoku;
 }
 
-SudokuValidator.prototype.validate = function(num){
+SudokuValidator.prototype.validate = function(num) {
   // Do work here
-}
+  let findOrder = n => {
+    let arr = [];
+    for (i = 1; i <= n; i++) {
+      arr.push(i);
+    }
+    return arr;
+  };
+
+  return this.sudoku.length !== num
+    ? false
+    : this.sudoku.every(
+        e => e.sort((a, b) => a - b).join() === findOrder(num).join()
+      )
+    ? true
+    : false;
+};
--- tests/Sudoku.spec.js
+++ tests/Sudoku.spec.js
@@ -1,52 +1,56 @@
-'use strict';
+"use strict";
 
-const Test = require('tape');
+const Test = require("tape");
 
-const Sudoku = require('./../Sudoku');
-const SudokuValidator = require('./../SudokuValidator').SudokuValidator;
+const Sudoku = require("./../Sudoku");
+const SudokuValidator = require("./../SudokuValidator").SudokuValidator;
 
-Test('Sudoku-Validator', suite => {
-  Test('3x3 Grid (9)', t => {
+Test("Sudoku-Validator", suite => {
+  Test("3x3 Grid (9)", t => {
     let sudoku = new SudokuValidator(Sudoku.nineTrue());
-    t.equal(sudoku.validate(9), true, 'Should be a valid Sudoku Grid');
+    t.equal(sudoku.validate(9), true, "Should be a valid Sudoku Grid");
     t.end();
   });
-  Test('Second 3x3 Grid (9)', t => {
+  Test("Second 3x3 Grid (9)", t => {
     let sudoku = new SudokuValidator(Sudoku.nineTrueTwo());
-    t.equal(sudoku.validate(9), true, 'Should also be a valid Sudoku Grid');
+    t.equal(sudoku.validate(9), true, "Should also be a valid Sudoku Grid");
     t.end();
   });
-  Test('Bad 3x3 Grid (9)', t => {
+  Test("Bad 3x3 Grid (9)", t => {
     let sudoku = new SudokuValidator(Sudoku.nineFalse());
-    t.equal(sudoku.validate(9), false, 'Should return false for invalid grid');
+    t.equal(sudoku.validate(9), false, "Should return false for invalid grid");
     t.end();
   });
-  Test('Badder 3x3 Grid (9)', t => {
+  Test("Badder 3x3 Grid (9)", t => {
     let sudoku = new SudokuValidator(Sudoku.nineFalseTwo());
-    t.equal(sudoku.validate(9), false, 'Should also return false for invalid grid');
+    t.equal(
+      sudoku.validate(9),
+      false,
+      "Should also return false for invalid grid"
+    );
     t.end();
   });
 
-  Test('2x2 Grid (4)', t => {
+  Test("2x2 Grid (4)", t => {
     let sudoku = new SudokuValidator(Sudoku.fourTrue());
-    t.equal(sudoku.validate(4), true, 'Should return true');
+    t.equal(sudoku.validate(4), true, "Should return true");
     t.end();
   });
-  Test('Bad 2x2 Grid (4)', t =>{
+  Test("Bad 2x2 Grid (4)", t => {
     let sudoku = new SudokuValidator(Sudoku.fourFalse());
-    t.equal(sudoku.validate(4), false, 'Should return false');
+    t.equal(sudoku.validate(4), false, "Should return false");
     t.end();
-  })
+  });
 
-  Test('4x4 Grid (16)', t => {
+  Test("4x4 Grid (16)", t => {
     let sudoku = new SudokuValidator(Sudoku.sixteenTrue());
-    t.equal(sudoku.validate(16), true,  'Should return true');
+    t.equal(sudoku.validate(16), true, "Should return true");
     t.end();
-  })
-  Test('Bad 4x4 Grid (16)', t => {
+  });
+  Test("Bad 4x4 Grid (16)", t => {
     let sudoku = new SudokuValidator(Sudoku.sixteenFalse());
-    t.equal(sudoku.validate(16), false, 'Should return false');
+    t.equal(sudoku.validate(16), false, "Should return false");
     t.end();
-  })
+  });
   suite.end();
 });
